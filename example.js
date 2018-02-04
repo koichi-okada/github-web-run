21216cbdba862049e6763b2b6b37723938f68d5e
morinkey <info@javatechnology.net>
2018-02-04 11:10:29 +0900

    First project.

---
+++ example.js
@@ -0,0 +1,8 @@
+let start = function(){
+    var element = document.getElementById( "main" ) ;
+    element.classList.add('load');
+}
+let stop = function(){
+    var element = document.getElementById( "main" ) ;
+    element.classList.remove('load');
+}
